5315994d6c8160b710a12962087c570971e8d688
cloudtronusa <cloudtronusa@gmail.com>
2026-06-10 02:24:49 -0700

    fixed minor issues

--- src/vs/workbench/browser/parts/dialogs/dialog.ts
+++ src/vs/workbench/browser/parts/dialogs/dialog.ts
@@ -48,7 +48,7 @@ export function createWorkbenchDialogOptions(options: Partial<IDialogOptions>, k
 export function createBrowserAboutDialogDetails(productService: IProductService): { title: string; details: string; detailsToCopy: string } {
 	const detailString = (useAgo: boolean): string => {
 		return localize('aboutDetail',
-			"Version: {0}\nCommit: {1}\nDate: {2}\nBrowser: {3}",
+			"Version: {0}\nCommit: {1}\nDate: {2}\nBrowser: {3}\n\nModified by @CloudTronUSA & @WorldEditAxe\nRepository: https://github.com/CloudTronUSA/vsprocessing",
 			productService.version || 'Unknown',
 			productService.commit || 'Unknown',
 			productService.date ? `${productService.date}${useAgo ? ' (' + fromNow(new Date(productService.date), true) + ')' : ''}` : 'Unknown',
